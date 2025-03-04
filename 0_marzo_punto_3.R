91df3017947b79fa8bd34946c62fa22d9553776a
Maiolino-Au <aurelio.maiolino@gmail.com>
2025-03-04 01:24:32 +0100

    Update 0_marzo_punto_3.R

--- 0_marzo_punto_3.R
+++ 0_marzo_punto_3.R
@@ -1,4 +1,6 @@
-setwd("C:/Users/Aurelio/Desktop/Organoidi_2024/")
+# setwd("C:/Users/Aurelio/Desktop/Organoidi_2024/")
+
+setwd("/home/user/Documents/organoidi_2024/Data - 3 - Organoidi_velasco")
 
 library(Seurat)
 library(tidyverse)
@@ -30,3 +32,32 @@ cores_ram(1,30)
 
 # _______________________________________________________________________________
 
+path_to_data <- paste("Data/expression_",timepoints[i],sep="")
+
+sc_data <- Read10X(data.dir = path_to_data, gene.column = 1) 
+# Create Seurat object
+sc_data <- CreateSeuratObject(counts = sc_data, min.cells = 3, min.features = 500, project = timepoints[i], names.delim = "-", names.field = 2)
+# Normalize the data
+sc_data <- NormalizeData(sc_data, normalization.method = "LogNormalize", scale.factor = 1e6)
+# Identify variable features
+sc_data <- FindVariableFeatures(sc_data, selection.method = "mvp", nfeatures = 2000)
+
+
+UMAP_coordinates <- read.delim(paste(getwd(),"/Data/cluster/umap_",timepoints[i],".txt",sep=""))
+UMAP_coordinates <- UMAP_coordinates[-1,]
+for (j in 2:7) {if (j!=4) {UMAP_coordinates[,j]<-as.numeric(UMAP_coordinates[,j])}}
+
+# Create a named vector of clusters
+cluster_map <- setNames(UMAP_coordinates$CellType, UMAP_coordinates$NAME)
+
+# Assign external clusters to the Seurat object
+sc_data$external_cluster <- cluster_map[colnames(sc_data)]
+
+# Set 'external_cluster' as the active identity
+Idents(sc_data) <- sc_data$external_cluster
+
+# Ensure 'external_cluster' is a factor with ordered levels
+sc_data$external_cluster <- factor(sc_data$external_cluster, levels = sort(unique(sc_data$external_cluster)))
+
+# Set active identity using the ordered levels
+Idents(sc_data) <- sc_data$external_cluster
