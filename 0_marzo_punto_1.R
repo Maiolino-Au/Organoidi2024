adaeaaee242ce816eceb46489c85934fcf7763da
Maiolino-Au <aurelio.maiolino@gmail.com>
2025-03-07 14:56:55 +0100

    Update 0_marzo_punto_1.R

--- 0_marzo_punto_1.R
+++ 0_marzo_punto_1.R
@@ -32,8 +32,8 @@ cores_ram(1,30)
 
 # _______________________________________________________________________________
 
-
-
+y<-1
+# Heatmap CellType X Cluster
 for (y in 1:length(timepoints)) {
   UMAP_coordinates <- read.delim(paste(getwd(),"/Data/cluster/umap_",timepoints[y],".txt",sep=""))
   UMAP_coordinates <- UMAP_coordinates[-1,]
@@ -69,7 +69,7 @@ for (y in 1:length(timepoints)) {
     # further customizing the heatmap by
     theme_minimal() + # applying colors and title
     
-    scale_fill_gradient(low="white", high="blue") + # setting gradient color as red and white
+    scale_fill_gradient(low="white", high="blue", limits = c(0, 1), breaks = seq(0, 1, by = 0.25)) + # setting gradient color as red and white
     
     # setting the title and subtitles using
     # title and subtitle
@@ -94,15 +94,85 @@ for (y in 1:length(timepoints)) {
   jpeg(paste(name_new_dir_0,"/",timepoints[y],"_Cluster&CellTypes.jpg",sep=""),width=1920, height=1080)
   print(heatmap_plot)
   dev.off()
+  
+  
+}
+y<-1
+# Heatmap Cluster X CellType
+for (y in 1:length(timepoints)) {
+  UMAP_coordinates <- read.delim(paste(getwd(),"/Data/cluster/umap_",timepoints[y],".txt",sep=""))
+  UMAP_coordinates <- UMAP_coordinates[-1,]
+  for (i in 2:7) {if (i!=4) {UMAP_coordinates[,i]<-as.numeric(UMAP_coordinates[,i])}}
+  
+  CellType <- sort(unique(UMAP_coordinates$CellType))
+  N_clusters <- max(as.numeric(UMAP_coordinates$Cluster))
+  
+  results <- matrix(0, ncol = length(CellType), nrow = N_clusters+1)
+  results <- data.frame(results)
+  rownames(results) <- 0:N_clusters
+  colnames(results) <- CellType
+  
+  print(timepoints[y])
+  for (Cl in 0:N_clusters) {
+    Tot <- length(UMAP_coordinates[UMAP_coordinates$Cluster==Cl,7])
+    for (CT in 1:length(CellType)) {
+      results[Cl+1,CT] <- length(UMAP_coordinates[UMAP_coordinates$CellType==CellType[CT] & UMAP_coordinates$Cluster==Cl,7])/Tot
+    }
+    print(paste(Cl,"-",sum(results[Cl,])>0.999))
+    
+  }
+  write.csv(results, file = paste(name_new_dir_0,"/",timepoints[y],"_Cluster&CellTypes_inverse.csv",sep=""))
+  
+  results <- results %>%
+    rownames_to_column() %>%
+    gather(colname, value, -rowname)
+  
+  results$rowname <- as.numeric(results$rowname)
+  
+  heatmap_plot <- ggplot(results,aes(colname, rowname, fill=value)) + 
+    geom_tile() +
+    
+    # further customizing the heatmap by
+    theme_minimal() + # applying colors and title
+    
+    scale_fill_gradient(low="white", high="green", limits = c(0, 1), breaks = seq(0, 1, by = 0.25)) + # setting gradient color as red and white
+    
+    # setting the title and subtitles using
+    # title and subtitle
+    labs(title = timepoints[y]) +
+    labs(subtitle = NULL) +
+    
+    labs(x =NULL, y =NULL) + # setting x and y labels using labs
+    
+    scale_y_continuous(labels = as.character(0:N_clusters), breaks = 0:N_clusters) +
+    
+    theme(
+      title = element_text(size = 30),
+      
+      axis.text = element_text(size = 20),
+      
+      axis.text.x = element_text(angle = 45, hjust = 1),
+      
+      legend.text = element_text(size = 25),
+      legend.title = element_text(size = 25),
+      
+      legend.key.size = unit(1, 'cm')
+    )
+  
+  jpeg(paste(name_new_dir_0,"/",timepoints[y],"_Cluster&CellTypes_inverse.jpg",sep=""),width=1920, height=1080)
+  print(heatmap_plot)
+  dev.off()
+  
+  
 }
 
 
 
-# _______________________________________________________________________________
+# ___TESTING____________________________________________________________________________
 
 
 
-y <- 2
+y <-1 
 UMAP_coordinates <- read.delim(paste(getwd(),"/Data/cluster/umap_",timepoints[y],".txt",sep=""))
 UMAP_coordinates <- UMAP_coordinates[-1,]
 for (i in 2:7) {if (i!=4) {UMAP_coordinates[,i]<-as.numeric(UMAP_coordinates[,i])}}
@@ -110,26 +180,35 @@ for (i in 2:7) {if (i!=4) {UMAP_coordinates[,i]<-as.numeric(UMAP_coordinates[,i]
 CellType <- sort(unique(UMAP_coordinates$CellType))
 N_clusters <- max(as.numeric(UMAP_coordinates$Cluster))
 
-results <- matrix(0, ncol = N_clusters+1, nrow = length(CellType))
+results <- matrix(0, ncol = length(CellType), nrow = N_clusters+1)
 results <- data.frame(results)
-rownames(results) <- CellType
-colnames(results) <- 0:N_clusters
+rownames(results) <- 0:N_clusters
+colnames(results) <- CellType
 
 print(timepoints[y])
-for (CT in 1:length(CellType)) {
-  Tot <- length(UMAP_coordinates[UMAP_coordinates$CellType==CellType[CT],7])
-  for (Cl in 0:N_clusters) {
-    results[CT,Cl+1] <- length(UMAP_coordinates[UMAP_coordinates$CellType==CellType[CT] & UMAP_coordinates$Cluster==Cl,7])/Tot
-  }
-  print(paste(CT,"-",sum(results[CT,])==1))
-}
+# for (CT in 1:length(CellType)) {
+#   Tot <- length(UMAP_coordinates[UMAP_coordinates$CellType==CellType[CT],7])
+#   for (Cl in 0:N_clusters) {
+#     results[CT,Cl+1] <- length(UMAP_coordinates[UMAP_coordinates$CellType==CellType[CT] & UMAP_coordinates$Cluster==Cl,7])/Tot
+#   }
+#   print(paste(CT,"-",sum(results[CT,])==1))
+#   
+# }
 
+for (Cl in 0:N_clusters) {
+  Tot <- length(UMAP_coordinates[UMAP_coordinates$Cluster==Cl,7])
+  for (CT in 1:length(CellType)) {
+    results[Cl+1,CT] <- length(UMAP_coordinates[UMAP_coordinates$CellType==CellType[CT] & UMAP_coordinates$Cluster==Cl,7])/Tot
+  }
+  print(paste(Cl,"-",sum(results[Cl,])>0.999))
+  
+}
 
 results <- results %>%
   rownames_to_column() %>%
   gather(colname, value, -rowname)
 
-results$colname <- as.numeric(results$colname)
+results$rowname <- as.numeric(results$rowname)
 
 heatmap_plot <- ggplot(results,aes(colname, rowname, fill=value)) + 
   geom_tile() +
@@ -137,7 +216,7 @@ heatmap_plot <- ggplot(results,aes(colname, rowname, fill=value)) +
   # further customizing the heatmap by
   theme_minimal() + # applying colors and title
   
-  scale_fill_gradient(low="white", high="blue", limits = c(0, 1), breaks = seq(0, 1, by = 0.25)) + # setting gradient color as red and white
+  scale_fill_gradient(low="white", high="green", limits = c(0, 1), breaks = seq(0, 1, by = 0.25)) + # setting gradient color as red and white
   
   # setting the title and subtitles using
   # title and subtitle
@@ -146,12 +225,14 @@ heatmap_plot <- ggplot(results,aes(colname, rowname, fill=value)) +
   
   labs(x =NULL, y =NULL) + # setting x and y labels using labs
   
-  scale_x_continuous(labels = as.character(0:N_clusters), breaks = 0:N_clusters) +
+  scale_y_continuous(labels = as.character(0:N_clusters), breaks = 0:N_clusters) +
   
   theme(
     title = element_text(size = 30),
     
     axis.text = element_text(size = 20),
+    
+    axis.text.x = element_text(angle = 45, hjust = 1),
 
     legend.text = element_text(size = 25),
     legend.title = element_text(size = 25),
@@ -166,3 +247,72 @@ print(heatmap_plot)
 dev.off()
 
 
+
+y<-1
+# Heatmap Cluster X CellType
+for (y in 1:length(timepoints)) {
+  UMAP_coordinates <- read.delim(paste(getwd(),"/Data/cluster/umap_",timepoints[y],".txt",sep=""))
+  UMAP_coordinates <- UMAP_coordinates[-1,]
+  for (i in 2:7) {if (i!=4) {UMAP_coordinates[,i]<-as.numeric(UMAP_coordinates[,i])}}
+  
+  CellType <- sort(unique(UMAP_coordinates$CellType))
+  N_clusters <- max(as.numeric(UMAP_coordinates$Cluster))
+  
+  results <- matrix(0, ncol = length(CellType), nrow = N_clusters+1)
+  results <- data.frame(results)
+  rownames(results) <- 0:N_clusters
+  colnames(results) <- CellType
+  
+  print(timepoints[y])
+  for (Cl in 0:N_clusters) {
+    Tot <- length(UMAP_coordinates[UMAP_coordinates$Cluster==Cl,7])
+    for (CT in 1:length(CellType)) {
+      results[Cl+1,CT] <- length(UMAP_coordinates[UMAP_coordinates$CellType==CellType[CT] & UMAP_coordinates$Cluster==Cl,7])/Tot
+    }
+    print(paste(Cl,"-",sum(results[Cl,])>0.999))
+    
+  }
+  write.csv(results, file = paste(name_new_dir_0,"/",timepoints[y],"_Cluster&CellTypes_inverse.csv",sep=""))
+  
+  results <- results %>%
+    rownames_to_column() %>%
+    gather(colname, value, -rowname)
+  
+  results$colname <- as.numeric(results$colname)
+  
+  heatmap_plot <- ggplot(results,aes(colname, rowname, fill=value)) + 
+    geom_tile() +
+    
+    # further customizing the heatmap by
+    theme_minimal() + # applying colors and title
+    
+    scale_fill_gradient(low="white", high="green", limits = c(0, 1), breaks = seq(0, 1, by = 0.25)) + # setting gradient color as red and white
+    
+    # setting the title and subtitles using
+    # title and subtitle
+    labs(title = timepoints[y]) +
+    labs(subtitle = NULL) +
+    
+    labs(x =NULL, y =NULL) + # setting x and y labels using labs
+    
+    scale_y_continuous(labels = as.character(0:N_clusters), breaks = 0:N_clusters) +
+    
+    theme(
+      title = element_text(size = 30),
+      
+      axis.text = element_text(size = 20),
+      
+      axis.text.x = element_text(angle = 45, hjust = 1),
+      
+      legend.text = element_text(size = 25),
+      legend.title = element_text(size = 25),
+      
+      legend.key.size = unit(1, 'cm')
+    )
+  
+  jpeg(paste(name_new_dir_0,"/",timepoints[y],"_Cluster&CellTypes_inverse.jpg",sep=""),width=1920, height=1080)
+  print(heatmap_plot)
+  dev.off()
+  
+  
+}
