cbff39a50b301eb776d49fbc0943332fb3dd8dfe
Maiolino-Au <aurelio.maiolino@gmail.com>
2025-03-17 22:41:14 +0100

    Update Named_3.R

--- Named_3.R
+++ Named_3.R
@@ -1,4 +1,6 @@
-setwd("/home/user/Documents/organoidi_2024/Data - 3 - Organoidi_velasco")
+setwd("C:/Users/Aurelio/Desktop/Organoidi_2024/")
+
+# setwd("/home/user/Documents/organoidi_2024/Data - 3 - Organoidi_velasco")
 
 library(Seurat)
 library(tidyverse)
@@ -25,17 +27,21 @@ if (dir.exists(name_new_dir_0)==FALSE) {
 timepoints <- c("23days","1month","1.5month","2month","3month","4month","5month","6month")
 genes_of_interest <- c("SRCIN1")
 
-cores_ram(1,30)
+cores_ram(1,64)
 
 
 
 
-i <- 5
+i <- 1
 
 print(paste(timepoints[i],"Starting"))
 
 path_to_data <- paste("Data/expression_",timepoints[i],sep="")
 
+# ___                     _________               ______               Prova |
+#                                                                            V 
+
+# name_new_dir_1 <- paste(name_new_dir_0,"/Named_CellType_",timepoints[i],"_Prova",sep="")
 name_new_dir_1 <- paste(name_new_dir_0,"/Named_CellType_",timepoints[i],sep="")
 if (dir.exists(name_new_dir_1)==FALSE) {
   dir.create(name_new_dir_1)
@@ -82,9 +88,9 @@ cluster_markers <- FindAllMarkers(sc_data,
 # Save
 save(cluster_markers, file = paste(name_new_dir_1,"/Named_",timepoints[i],"_CellType_markers.Robj", sep=""))
 
-top_markers_all <- cluster_markers %>% group_by(cluster) %>% top_n(n = 30, wt = avg_log2FC)
+top_markers_all <- cluster_markers %>% group_by(cluster) %>% top_n(n = 100, wt = avg_log2FC)
 # Save
-write.csv(top_markers_all, file = paste(name_new_dir_1,"/Named_",timepoints[i],"_top_30_markers_CellType.csv",sep=""))
+write.csv(top_markers_all, file = paste(name_new_dir_1,"/Named_",timepoints[i],"_top_100_markers_CellType.csv",sep=""))
 
 # Filter markers for genes of interest
 de_genes <- cluster_markers %>% filter(gene %in% genes_of_interest)
