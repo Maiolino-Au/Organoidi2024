dee875654584fd2fc47c04fdbfd3f8094809cb39
Aurelio Maiolino <aurelio.maiolino@gmail.com>
2025-05-09 17:16:26 +0200

    Update R_script.R

--- Paper_clusters_from_scratch/R_script.R
+++ Paper_clusters_from_scratch/R_script.R
@@ -17,54 +17,68 @@ path_to_data <- "/sharedFolder/data_organoidi_velasco/Data/"
 
 # sc_data <- ""
 
-name_new_dir_0 <- paste(getwd(),"/Results",sep="")
-if (!dir.exists(name_new_dir_0)) {
-  dir.create(name_new_dir_0)
+name_new_dir_results <- paste(getwd(),"/Results",sep="")
+if (!dir.exists(name_new_dir_results)) {
+    dir.create(name_new_dir_results)
 } 
 
 name_new_dir_partial <- paste(getwd(),"/Partial",sep="")
 if (!dir.exists(name_new_dir_partial)) {
-  dir.create(name_new_dir_partial)
+    dir.create(name_new_dir_partial)
 } 
 
 
-load_data <- function(time_point){
-  # Load the data
-  sc_data <- Read10X(data.dir = paste(path_to_data, "expression_", timepoints[time_point], sep=""), gene.column = 1)
-  
-  # Create Seurat object
-  sc_data <- CreateSeuratObject(counts = sc_data, min.cells = 3, min.features = 500, project = timepoints[time_point], names.delim = "-", names.field = 2)
-    
-  # Normalize the data
-  sc_data <- NormalizeData(sc_data, normalization.method = "LogNormalize", scale.factor = 1e6)
-  
-  # Find variable features
-  sc_data <- FindVariableFeatures(sc_data, selection.method = "mvp", nfeatures = 2000)
+load.data <- function(time_point){
+    print(paste("Loading data for time point:", timepoints[time_point]))
 
-  # Scale the data
-  sc_data <- ScaleData(sc_data)
-  
-  return(sc_data)
+    # Load the data
+    sc_data <- Read10X(data.dir = paste(path_to_data, "expression_", timepoints[time_point], sep=""), gene.column = 1)
+
+    # Create Seurat object
+    sc_data <- CreateSeuratObject(counts = sc_data, min.cells = 3, min.features = 500, project = timepoints[time_point], names.delim = "-", names.field = 2)
+    
+    # Normalize the data
+    sc_data <- NormalizeData(sc_data, normalization.method = "LogNormalize", scale.factor = 1e6)
+    
+    # Find variable features
+    sc_data <- FindVariableFeatures(sc_data, selection.method = "mvp", nfeatures = 2000)
+
+    # Scale the data
+    sc_data <- ScaleData(sc_data)
+    
+    return(sc_data)
 }
 
 # PCA&Clusterization 
-PCA_cluster <- function(sc_data, res){
-  # PCA
-  sc_data <- RunPCA(sc_data, npcs = 50, verbose = FALSE)
-  #print(ElbowPlot(object = sc_data, ndims = 50))
+PCA.cluster <- function(sc_data, res){
+    print(paste("Running PCA and clustering for time point:", timepoints[time_point]))
     
-  # Cluster the cells
-  sc_data <- FindNeighbors(sc_data, dims = 1:40)
-  sc_data <- FindClusters(sc_data, resolution = res)
-  
-  print(table(Idents(sc_data)))
+    # PCA
+    sc_data <- RunPCA(sc_data, npcs = 50, verbose = FALSE)
+    #print(ElbowPlot(object = sc_data, ndims = 50))
 
-  save(sc_data, file = paste(name_new_dir_partial, "/PCA_res_",res,"_",timepoints[time_point],".Robj", sep=""))
-  return(sc_data)
+    # Cluster the cells
+    sc_data <- FindNeighbors(sc_data, dims = 1:40)
+    sc_data <- FindClusters(sc_data, resolution = res)
+    
+    print(table(Idents(sc_data)))
+
+    # Save the PCA plot
+    name_new_dir <- paste(name_new_dir_partial, "/", timepoints[time_point], sep="")
+    if (!dir.exists(name_new_dir)) {
+        dir.create(name_new_dir)
+    } 
+
+    print(paste("Saving PCA for time point", timepoints[time_point], "in", name_new_dir))
+    save(sc_data, file = paste(name_new_dir, "/PCA_res_",res,"_",timepoints[time_point],".Robj", sep=""))
+
+    return(sc_data)
 }
 
 # FIND ALL MARKERS
-cluster_markers <- function(sc_data) {
+cluster.markers <- function(sc_data) {
+    print(paste("Finding all markers for time point:", timepoints[time_point]))
+
     # Find all markers for every cluster compared to all remaining cells
     cluster_markers <- FindAllMarkers(sc_data,
                                         only.pos = TRUE,   # Considera solo i marker espressi positivamente
@@ -72,49 +86,77 @@ cluster_markers <- function(sc_data) {
                                         logfc.threshold = 0.25)  # Soglia minima di LogFC
     
     # Save the markers
-    save(cluster_markers, file = paste(name_new_dir_partial, "/cluster_markers_",timepoints[time_point],".Robj", sep=""))
+    name_new_dir <- paste(name_new_dir_partial, "/", timepoints[time_point], sep="")     
+    if (!dir.exists(name_new_dir)) {
+        dir.create(name_new_dir)
+    } 
+    
+    print(paste("Saving cluster markers for time point", timepoints[time_point], "in", name_new_dir))
+    save(cluster_markers, file = paste(name_new_dir, "/cluster_markers_",timepoints[time_point],".Robj", sep=""))
+
     return(cluster_markers)
 }
 
 # FIND DIFFERENTIALLY EXPRESSED GENES
-de_genes <- function(genes_oi){
+de.genes <- function(genes_oi){
+    print(paste("Finding differentially expressed genes for time point:", timepoints[time_point]))
+
     # Find differentially expressed genes
-    provv <- cluster_markers %>% filter(gene %in% genes_of_interest) 
-    print(provv)
-    # Save the DE genes
-    write.csv(provv, file = paste(name_new_dir_partial, "/de_genes_",timepoints[time_point],".csv", sep=""))
+    de_genes <- cluster_markers %>% filter(gene %in% genes_of_interest) 
+    print(de_genes)
     
-    return(provv)
+    # Save the DE genes
+    name_new_dir <- paste(name_new_dir_results, "/", timepoints[time_point], sep="")     
+    if (dir.exists(name_new_dir)) {
+        dir.create(name_new_dir)
+    }  
+
+    print(paste("Saving differentially expressed genes for time point", timepoints[time_point], "in", name_new_dir))
+    write.csv(de_genes, file = paste(name_new_dir, "/de_genes_",timepoints[time_point],".csv", sep=""))
+    
+    return(de_genes)
 }
 
 
-do_iteration <- function(time_point){
-  # Load the data
-  sc_data <- load_data(time_point)
-  
-  # Run PCA and clustering
-  sc_data <- PCA_cluster(sc_data, res = 0.5)
-  
-  # Find all markers
-  cluster_markers <- cluster_markers(sc_data)
-  
-  # Find differentially expressed genes
-  de_genes <- de_genes(genes_of_interest)
-}
+# MAIN FUNCTION
+print(paste("Processing time point:", timepoints[time_point]))
+
+# LOAD LIBRARIES
+library(Seurat)
+library(tidyverse)
+library(future)
+library(ggplot2)
+library(dplyr)
+library(presto)
+#library(enrichR)
+library(cowplot)
+
+# Load the data
+sc_data <- load.data(time_point)
+
+# Run PCA and clustering
+sc_data <- PCA.cluster(sc_data, res = 1)
+
+# Find all markers
+cluster_markers <- cluster.markers(sc_data)
+
+# Find differentially expressed genes
+de_genes <- de.genes(genes_of_interest)
+
 
 
 
 # Plotting
 plotting <- function(sc_data, genes_oi){
-  # Plot the expression of the genes of interest
-  for (gene in genes_oi) {
+    # Plot the expression of the genes of interest
+    for (gene in genes_oi) {
     p1 <- FeaturePlot(sc_data, features = gene, cols = c("lightgrey", "blue"), pt.size = 0.5) + 
-      ggtitle(paste("Expression of", gene)) + 
-      theme_minimal()
+        ggtitle(paste("Expression of", gene)) + 
+        theme_minimal()
     
-    # Save the plot
-    ggsave(filename = paste(name_new_dir_partial, "/FeaturePlot_", gene, "_", timepoints[time_point], ".png", sep=""), plot = p1)
-  }
+        # Save the plot
+        ggsave(filename = paste(name_new_dir_partial, "/FeaturePlot_", gene, "_", timepoints[time_point], ".png", sep=""), plot = p1)
+    }
 }
 
 
