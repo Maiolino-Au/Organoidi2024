a177632d1957b5a167fa08988ec35d9ca9e65e21
Aurelio Maiolino <aurelio.maiolino@gmail.com>
2025-10-16 14:24:08 +0200

    Enhance run function with data processing steps
    Added data loading, preprocessing, and clustering steps in the run function.

--- SETTEMBRE/6_run.r
+++ SETTEMBRE/6_run.r
@@ -12,11 +12,44 @@ run.month <- function (
     resolution = 1,
     dimensions = c(1:20)
 ) {
-    
+    # Create directory for specific file
+    dir_results = paste0(dir_save, "Res_", file_name, "/") %>% dir.create(recursive = T, showWarnings = F)
+
+    # Load data
+    raw_data <- load.data(file_name = file_name,
+                         data_path = path_to_data)
+
+    # Preprocessing
+    sc_data <- preprocessing(data = raw_data,
+                            file_name = file_name)
+
+    # Clustering
+    sc_data <- PCA.cluster(data, 
+                           file_name, 
+                           res = 1, 
+                           n_dim = 20, 
+                           save_dir = dir_save,
+                           save_add_on = "",)
+
+    # Markers
+
+    # Differentially expressed genes
+
+    # Plots, no annotation
+
+    # Annotation
+
+    # Subclustering
+
+    # Markers of subclusters
+
+    # Differentially expressed genes
+
+    # Plots
 }
 
 
-ref <- 
+ref <- readH5AD("/sharedFolder/public-anndata-project_570_1__published__cKUH0kM97x2oczRNPkqvk_1265.h5ad")
 
 for (i in timepoints) {
     run.month(file_name = i)
