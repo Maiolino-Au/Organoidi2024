01c2039fe75fe1337070401a4382312d5b83ec61
Aurelio Maiolino <aurelio.maiolino@gmail.com>
2025-10-16 19:00:28 +0200

    Refactor run.month function and improve directory handling

--- SETTEMBRE/6_run.r
+++ SETTEMBRE/6_run.r
@@ -1,9 +1,9 @@
 source(file = "6_functions.r")
 
-timepoints = c("1month","2month", "3month")
-dir_save = "/sharedFolder/Results/" %>% dir.create(recursive = T, showWarnings = F)
+timepoints <- c("1month", "2month", "3month")
+dir_save <- "/sharedFolder/Results/" 
 
-run.month <- function (
+run.month <- function(
     file_name,
     ref = ref,
     genes_of_interest = c("SRCIN1", "SATB2"),
@@ -12,45 +12,134 @@ run.month <- function (
     resolution = 1,
     dimensions = c(1:20)
 ) {
+    tic()
     # Create directory for specific file
-    dir_results = paste0(dir_save, "Res_", file_name, "/") %>% dir.create(recursive = T, showWarnings = F)
+    dir.create(dir_save, recursive = T, showWarnings = F)
+    dir_results <- paste0(dir_save, "Res_", file_name, "/") %>% dir.create(recursive = T, showWarnings = F)
 
     # Load data
-    raw_data <- load.data(file_name = file_name,
-                         data_path = path_to_data)
+    raw_data <- load.data(
+        file_name = file_name,
+        data_path = path_to_data
+    )
+
+    print("Preprocessing")
 
     # Preprocessing
-    sc_data <- preprocessing(data = raw_data,
-                            file_name = file_name)
+    sc_data <- preprocessing(
+        data = raw_data,
+        file_name = file_name
+    )
+
+    print("Clustering")
 
     # Clustering
-    sc_data <- PCA.cluster(data, 
-                           file_name, 
-                           res = 1, 
-                           n_dim = 20, 
-                           save_dir = dir_save,
-                           save_add_on = "",)
+    sc_data <- PCA.cluster(data = sc_data,
+        file_name = file_name,
+        res = resolution,
+        n_dim = dimensions,
+        dir_save = dir_results,
+        save_add_on = ""
+    )
+
+    print("Markers")
 
     # Markers
+    cluster_markers <- cluster.markers(data = sc_data)
+    write.csv(cluster_markers, file = paste0(dir_results, "/markers_", file_name, ".csv"))
 
+    print("de_genes")
+    
     # Differentially expressed genes
+    de_genes <- cluster_markers %>% filter(gene %in% genes_of_interest) %>% arrange(avg_log2FC) %>% filter(p_val_adj < 0.05 & avg_log2FC > 0.6) 
+    write.csv(de_genes, file = paste0(dir_results, "/de_genes_", file_name, ".csv"))
 
+    print("Plots")
+    
     # Plots, no annotation
+    plottamelo.tutto(
+        data = sc_data,
+        file_name = file_name,
+        genes_of_interest = genes_of_interest,
+        dir_save = dir_results,
+        save_add_on = "no_annotation",
+        pt.size = 1,
+        print_plot = F
+    )
+
+    filtered_obj <- subset(sc_data, subset = seurat_clusters %in% de_genes$cluster)
+    plottamelo.tutto(
+        data = filtered_obj,
+        file_name = file_name,
+        genes_of_interest = genes_of_interest,
+        dir_save = dir_results,
+        save_add_on = "filtered",
+        pt.size = 1,
+        print_plot = F
+    )
 
     # Annotation
 
+    print("Sub-clustering")
+    
     # Subclustering
+    sc_data <- PCA.cluster(data = filtered_obj,
+        file_name = file_name,
+        res = resolution,
+        n_dim = dimensions,
+        dir_save = dir_results,
+        save_add_on = "filterd"
+    )
 
+    print("Sub-markers")
+    
     # Markers of subclusters
+    cluster_markers <- cluster.markers(data = filtered_obj)
+    write.csv(cluster_markers, file = paste0(dir_results, "/markers__filtered_", file_name, ".csv"))
+
+    print("Sub-de_genes")
 
     # Differentially expressed genes
+    de_genes <- cluster_markers %>% filter(gene %in% genes_of_interest) %>% arrange(avg_log2FC) %>% filter(p_val_adj < 0.05 & avg_log2FC > 0.6) 
+    write.csv(de_genes, file = paste0(dir_results, "/de_genes_", file_name, ".csv"))
 
+    print("Sub-plots")
+    
     # Plots
+    plottamelo.tutto(
+        data = sc_data,
+        file_name = file_name,
+        genes_of_interest = genes_of_interest,
+        dir_save = dir_results,
+        save_add_on = "no_annotation_filtered",
+        pt.size = 1,
+        print_plot = F
+    )
+
+    filtered_obj <- subset(filtered_obj, subset = seurat_clusters %in% de_genes$cluster)
+    plottamelo.tutto(
+        data = filtered_obj,
+        file_name = file_name,
+        genes_of_interest = genes_of_interest,
+        dir_save = dir_results,
+        save_add_on = "filtered_squared",
+        pt.size = 1,
+        print_plot = F
+    )
+
+    elapsed <- toc(log = TRUE, quiet = T)
+    return(total_time(elapsed$toc - elapsed$tic))
 }
 
+#library(reticulate)
+#use_python("/usr/bin/python3", required = TRUE)
+#library(zellkonverter)
 
-ref <- readH5AD("/sharedFolder/public-anndata-project_570_1__published__cKUH0kM97x2oczRNPkqvk_1265.h5ad")
+#HNOA <- readH5AD("/SingleR_reference/HNOA.h5ad", use_anndata = FALSE)
 
-for (i in timepoints) {
-    run.month(file_name = i)
-}
+
+
+#for (i in timepoints) {
+#    run.month(file_name = i, 
+#              dir_save = "/sharedFolder/Paper_clusters_from_scratch/0_September/plots_oct/")
+#}
