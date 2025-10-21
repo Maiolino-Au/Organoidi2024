d7f00731abcdc742844613c2994a3e81363ea113
Aurelio Maiolino <aurelio.maiolino@gmail.com>
2025-10-21 18:22:13 +0200

    Add files via upload

---
+++ SETTEMBRE/6_functions.r
@@ -0,0 +1,154 @@
+# LOAD LIBRARIES
+suppressPackageStartupMessages(library(Seurat))
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(future))
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(presto))
+suppressPackageStartupMessages(library(cowplot))
+suppressPackageStartupMessages(library(tictoc))
+
+suppressPackageStartupMessages(library(SingleCellExperiment))
+suppressPackageStartupMessages(library(SingleR))
+suppressPackageStartupMessages(library(scuttle))
+suppressPackageStartupMessages(library(zellkonverter))
+suppressPackageStartupMessages(library(patchwork))
+suppressPackageStartupMessages(library(SeuratDisk))
+
+
+total_time <- function(seconds) {
+    d <- seconds %/% (86400)
+    h <- (seconds %% 86400) %/% 3600
+    m <- (seconds %% 3600) %/% 60
+    s <- seconds %% 60
+    
+    cat(sprintf("Total Time: %f Days, %f Hours, %f Minutes and %f Seconds\n", d, h, m, s))
+}
+
+load.data <- function(
+    file_name,
+    data_path = path_to_data
+) {
+    # Load the data
+    print(paste(data_path, "expression_", file_name, sep = ""))
+    data <- Read10X(data.dir = paste(data_path, "expression_", file_name, sep = ""), gene.column = 1)
+
+    return(data)
+}
+
+preprocessing <- function(
+    data,
+    file_name,
+    output = F
+) {
+    # Create Seurat object
+    data <- CreateSeuratObject(counts = data, min.cells = 3, min.features = 500, project = file_name, names.delim = "-", names.field = 2)
+
+    mito.genes <- grep(pattern = "^MT-", x = rownames(x = data), value = TRUE)
+    percent.mito <- Matrix::colSums(GetAssayData(data, slot='counts')[mito.genes, ]) / Matrix::colSums(GetAssayData(data, slot="counts"))
+    data[['percent.mito']] <- percent.mito
+    ribo.genes <- grep("^RP[S,L]",rownames(data), value = TRUE)
+    percent.ribo <- Matrix::colSums(GetAssayData(data, slot='counts')[ribo.genes, ]) / Matrix::colSums(GetAssayData(data, slot="counts"))
+    data[['percent.ribo']] <- percent.ribo
+
+    # Normalize the data
+    data <- NormalizeData(data, normalization.method = "LogNormalize", scale.factor = 1e6, verbose = output)
+
+    # Find variable features
+    data <- FindVariableFeatures(data, selection.method = "mvp", nfeatures = 2000, verbose = output)
+
+    data = CellCycleScoring(data, s.features = cc.genes$s.genes, g2m.features = cc.genes$g2m.genes)
+    data$CC.Difference <- data$S.Score - data$G2M.Score
+
+    # Scale the data
+    data<-ScaleData(data, features=VariableFeatures(data),vars.to.regress=c("nCount_RNA","CC.Difference"), verbose = output)
+
+    return(data)
+}
+
+PCA.cluster <- function(
+    data, 
+    file_name,
+    res = 1, 
+    n_dim = 1:20, 
+    dir_save = F,
+    save_add_on = "",
+    output = F   
+) {
+    # PCA
+    data <- RunPCA(data, npcs = max(n_dim), verbose = output)
+    data <- RunUMAP(data, dims = n_dim, verbose = output)
+    #print(ElbowPlot(object = data, ndims = max(50, n_dim)))
+
+    # Cluster the cells
+    data <- FindNeighbors(data, dims = n_dim, verbose = output)
+    data <- FindClusters(data, resolution = res, verbose = output)
+    
+    #print(table(Idents(data)))
+
+    # Save the parial
+    if (dir_save != F) {
+        saveRDS(data, file = paste0(dir_save, "/Clustered_", file_name, "_", save_add_on, ".rds"))
+    }
+    
+    return(data)
+}
+
+plottamelo.tutto <- function(
+    data,
+    file_name,
+    genes_of_interest = genes_of_interest,
+    dir_save = dir_save,
+    save_add_on = "",
+    pt.size = 1,
+    width = 1920*2,
+    height = 1920*2,
+    scale.dimensions = 1,
+    print_plot = F
+) { 
+    umap <- DimPlot(data, reduction = "umap", label = TRUE, pt.size = pt.size) + 
+    ggtitle(paste("UMAP -",file_name))
+
+    gene_plot <- FeaturePlot(data, features = genes_of_interest) &
+    theme(aspect.ratio = 1)
+
+    plot_blend <- umap | gene_plot 
+
+    width = width*scale.dimensions
+    height = height*scale.dimensions
+    
+    if(save_add_on != "") {file_name = paste(file_name, save_add_on, sep = "_")}
+    
+    ggsave(filename = paste0(dir_save, "Plot_", file_name,"_umap.png"), 
+           plot = umap, 
+           width = width, height = height, units = "px")
+    ggsave(filename = paste0(dir_save, "Plot_", file_name, "_", genes_of_interest,"_umap.png"), 
+           plot = gene_plot, 
+           width = width, height = height, units = "px")
+    ggsave(filename = paste0(dir_save, "Plot_", file_name, "_combined_umap&", genes_of_interest, ".png"), 
+           plot = plot_blend, 
+           width = width*2, height = height, units = "px")
+}
+
+cluster.markers <- function(
+    data,
+    output = F
+) {
+    # Find all markers for every cluster compared to all remaining cells
+    markers <- FindAllMarkers(data,
+                              only.pos = TRUE,   # Considera solo i marker espressi positivamente
+                              min.pct = 0.25,    # Percentuale minima di espressione nelle cellule del cluster
+                              logfc.threshold = 0.25,  # Soglia minima di LogFC
+                              verbose = output)
+        
+    return(markers)
+}
+
+top.genes <- function(
+    clmark = cluster_markers,
+    num = 50
+) {
+    top_genes_by_cluster <- cluster_markers %>% group_by(cluster) %>% top_n(n = top_n_genes, wt = avg_log2FC) %>% as.data.frame()
+
+    return(top_genes_by_cluster)
+}
---
+++ SETTEMBRE/6_run.r
@@ -0,0 +1,99 @@
+source(file = "6_functions.r")
+
+timepoints <- c("1month", "2month", "3month")
+dir_save <- "/sharedFolder/Results/" 
+
+run.month <- function(
+    file_name,
+    ref = "",
+    genes_of_interest = c("SRCIN1", "SATB2"),
+    path_to_data = "/sharedFolder/Data/",
+    dir_save = dir_save,
+    resolution = 1,
+    dimensions = c(1:20),
+    output = F
+) {
+    tic()
+    # Create directory for specific file
+    dir.create(dir_save, recursive = T, showWarnings = F)
+    dir_results <- paste0(dir_save, "Res_", file_name, "/")
+    dir.create(dir_results, recursive = TRUE, showWarnings = FALSE)
+
+    # Load data
+    raw_data <- load.data(
+        file_name = file_name,
+        data_path = path_to_data
+    )
+
+    print("Preprocessing")
+
+    # Preprocessing
+    sc_data <- preprocessing(
+        data = raw_data,
+        file_name = file_name,
+        output = output
+    )
+
+    print("Clustering")
+
+    # Clustering
+    sc_data <- PCA.cluster(data = sc_data,
+        file_name = file_name,
+        res = resolution,
+        n_dim = dimensions,
+        dir_save = dir_save,
+        save_add_on = "",
+        output = output
+    )
+
+    print("Markers")
+
+    # Markers
+    cluster_markers <- cluster.markers(data = sc_data,
+        output = output)
+    write.csv(cluster_markers, file = paste0(dir_results, "/markers_", file_name, ".csv"))
+
+    print("de_genes")
+    
+    # Differentially expressed genes
+    de_genes <- cluster_markers %>% filter(gene %in% genes_of_interest) %>% arrange(avg_log2FC) %>% filter(p_val_adj < 0.05 & avg_log2FC > 0.6) 
+    write.csv(de_genes, file = paste0(dir_results, "/de_genes_", file_name, ".csv"))
+
+    print("Plots")
+    
+    # Plots, no annotation
+    plottamelo.tutto(
+        data = sc_data,
+        file_name = file_name,
+        genes_of_interest = genes_of_interest,
+        dir_save = dir_results,
+        save_add_on = "no_annotation",
+        pt.size = 1,
+        print_plot = F
+    )
+
+    # Annotation
+
+    
+
+    elapsed <- toc(log = TRUE, quiet = T)
+    return(total_time(elapsed$toc - elapsed$tic))
+}
+
+#library(reticulate)
+#use_python("/usr/bin/python3", required = TRUE)
+#library(zellkonverter)
+
+ref <- LoadH5Seurat("/SingleR_reference/HNOA_2_processed.h5seurat")
+
+
+
+for (i in timepoints) {
+    run.month(
+        file_name = i, 
+        dir_save = "/sharedFolder/plots_oct/",
+        ref = ref,
+        genes_of_interest = "SRCIN1",
+        path_to_data = "/sharedFolder/Data/",
+    )
+}
